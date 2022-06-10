1cf65fa316cd9393d05d09161114f99dffce0b8b
Danial Mecja <danialmecja@gmail.com>
2022-06-10 15:31:00 +0800

    Change imports

---
+++ rev-preprocessing.py
@@ -0,0 +1,9 @@
+# Imports
+import numpy as np
+import pandas as pd
+import nltk
+
+
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+stopword=nltk.corpus.stopwords.words('english')
