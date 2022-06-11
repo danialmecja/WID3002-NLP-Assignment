c101231acfcae8581bde4979a8c52bb013306dd5
Nurul Hanina <58135343+nurulhanina@users.noreply.github.com>
2022-06-11 23:17:33 +0800

    Add files via upload

---
+++ NLP_Amity.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+nltk.download('wordnet')
+nltk.download('stopwords')
+stopword=nltk.corpus.stopwords.words('english')
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+
+
+url="https://github.com/danmecj/WID3002-NLP-Assignment/raw/main/Extracted%20Reviews_V2.csv"
+#Read Raw
+df=pd.read_csv(url)
+
+
+
+#Preprocessing
+#Remove noise
+df['Preprocessed']=df['text'].str.replace("@", "") 
+df['Preprocessed']=df['text'].str.replace(r"http\S+", "") 
+df['Preprocessed']=df['text'].str.replace("[^a-zA-Z]", " ")
+
+def stopWord(text):
+    preprocessWord=' '.join([i for i in text.split() if i not in stopword])
+    return preprocessWord
+
+df['Preprocessed']=df['Preprocessed'].apply(lambda text : stopWord(text.lower()))
+df['Preprocessed']=df['Preprocessed'].apply(lambda text : nltk.word_tokenize(text))
+
+lemmatizer = nltk.stem.WordNetLemmatizer()
+
+def lemmatize_text(text):
+    return [lemmatizer.lemmatize(i) for i in text]
+
+df['Preprocessed']=df['Preprocessed'].apply(lambda text : lemmatize_text(text))
+
+column=['Unnamed:','name','locality','address','rating','text']
+selectlist=[x for x in df.columns if x not in column]
+datatowrite=df[selectlist]
+
+datatowrite.to_csv('Preprocessed_Text.csv', encoding='utf-8',index=False)
