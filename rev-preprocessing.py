32533e7bb8c935cc52d7e8fb227ffb45dc457e25
Danial Mecja <danialmecja@gmail.com>
2022-06-14 08:53:19 +0800

    Add example preprocessing methods

--- rev-preprocessing.py
+++ rev-preprocessing.py
@@ -3,7 +3,14 @@ import numpy as np
 import pandas as pd
 import nltk
 
-
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 stopword=nltk.corpus.stopwords.words('english')
+
+# Pandas
+df = pd.read_csv("Extracted Reviews_V2.csv")
+#Remove noise
+df['Preprocessed']=df['text'].str.replace("@", "") 
+df['Preprocessed']=df['text'].str.replace(r"http\S+", "") 
+df['Preprocessed']=df['text'].str.replace("[^a-zA-Z]", " ")
+print(df)
